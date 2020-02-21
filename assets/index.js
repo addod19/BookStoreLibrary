010f023be69eaf0ce24db416a18bf07cacad2042
Daniel <addodaniellarbi@gmail.com>
2020-02-21 10:52:58 +0000

    add functionality to dispaly and hide form upon clicking add book button

--- assets/index.js
+++ assets/index.js
@@ -7,4 +7,18 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function addBookToLibrary() {}
+function addBookToLibrary() {
+
+}
+
+document.querySelector('#submit').addEventListener('click', (e) => {
+  
+  const toggleForm = document.querySelector('.hidden-form');
+  // toggleForm.style.display = 'block';
+  if (toggleForm.style.display === 'none') {
+    toggleForm.style.display = 'block';
+  } else {
+    toggleForm.style.display = 'none';
+  }
+  // alert('hello');
+});
