413e9831fa6d14567b3fc6f8a7f0eb75a166ba44
macnick <nick@macexperts.gr>
2020-02-24 16:28:32 +0200

    Fix the form clear

--- assets/test.js
+++ assets/test.js
@@ -1,4 +1,4 @@
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read = 'Unread') {
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -6,38 +6,36 @@ function Book(title, author, pages, read) {
 }
 
 let bookList = {
-  books: [new Book('Twist Of The Wrist', 'Keith Code', 135, true),
-  new Book('A Perfect Day', 'Lady Teresa', 40, false),],
-  displayBooks: function () {
-    this.books.forEach(book => {
-      console.log([book.title, book.author, book.pages, book.read ? "Read" : "Unread"].join(', '));
-    });
-  },
+  books: [],//[new Book('Twist Of The Wrist', 'Keith Code', 135, true),
+  //new Book('A Perfect Day', 'Lady Teresa', 40, false)],
   addBook: function () {
-    let title = document.getElementById('title');
-    // author = document.getElementById('author'),
-    // pages = document.getElementById('pages'),
-    // read = document.getElementById('read');
+    let title = document.getElementById('title'),
+      author = document.getElementById('author'),
+      pages = document.getElementById('pages'),
+      read = document.getElementById('read-status');
 
-    this.books.push(new Book(title.value, null, null));
+    this.books.push(new Book(title.value, author.value, pages.value, read.value ? 'Read' : 'Unread'));
     title.value = "";
-    this.displayBooks();
+    author.value = "";
+    pages.value = "";
+    read.value = "";
+    view.displayBooks();
   },
   changeBook: function (position, title, author, pages, read) {
     title ? this.books[position].title = title : null;
     author ? this.books[position].author = author : null;
     pages ? this.books[position].pages = pages : null;
     this.books[position].read = read;
-    this.displayBooks();
+    view.displayBooks();
   },
   deleteBook: function (position) {
     this.books.splice(position, 1);
-    this.displayBooks();
+    view.displayBooks();
   },
   toggleRead: function (position) {
     let book = this.books[position]
     book.read = !book.read;
-    this.displayBooks();
+    view.displayBooks();
   },
   toggleAll: function () {
     if (this.books.every(book => book.read)) {
@@ -45,7 +43,7 @@ let bookList = {
     } else {
       this.books.forEach(book => book.read = true)
     }
-    this.displayBooks();
+    view.displayBooks();
   }
 };
 
@@ -58,19 +56,33 @@ let view = {
       let row = document.createElement('tr');
       let html = "";
       for (let key in list[i]) {
-        html += `<td>${list[i][key]}</td>`;
+        let id = "";
+        if (key == 'read') {
+          id = `data-id=${i}`
+        }
+        html += `<td ${id}>${list[i][key]}</td>`;
       }
       row.innerHTML = html;
       table.appendChild(row);
+      this.hideForm();
     }
+  },
+  showForm: () => {
+    f = document.getElementsByClassName('hidden-form');
+    f[0].style.display = 'block';
+  },
+  hideForm: () => {
+    f = document.getElementsByClassName('hidden-form');
+    f[0].style.display = 'none';
   }
 }
 
+document.querySelector("#add-book").addEventListener("click", function (event) {
+  event.preventDefault();
+  bookList.addBook();
+}, false);
 
-const showForm = () => {
-  f = document.getElementsByClassName('hidden-form');
-  f[0].style.display = 'block';
-}
+view.displayBooks();
 
 // let addButton = document.getElementById('submit');
 // addButton.addEventListener("click", () => bookList.displayBooks());
